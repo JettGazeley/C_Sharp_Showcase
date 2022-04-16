0f36dd9a83200b086967d788ab8b789f0a41b0a2
JettGazeley <jettgazeley@gmail.com>
2022-04-16 00:32:35 -0400

    Add project files.

---
+++ Restaurant.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantClass
+{
+    public class Restaurant
+    // Restaurant Class that includes three pieces of information: name, address and gratuity rate.
+    {
+        // Instance variables
+        private string name;
+        private string address;
+        private double gratuityRate;
+
+
+        // Properties with get and set for all instance variables
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public double GratuityRate
+        {
+            get { return gratuityRate; }
+
+            // Gratuity rate cannot be negative
+            set
+            {
+                if (value > 0)
+                {
+                    gratuityRate = value;
+                }
+            }
+        }
+
+
+        public Restaurant(string name, string address, double gratuityRate)
+        // Constructor to initalize the three instance variables
+        {
+            Name = name;
+            Address = address;
+            GratuityRate = gratuityRate;
+        }
+
+
+
+        public void GenerateReceipt()
+        // Generate Receipt method used to generate sales receipt
+        {
+            double price = 0;
+            double subtotal = 0;
+            double gratuityAmount = 0;
+            double grandTotal = 0;
+            int people = 0;
+
+            while (price != -1)
+            {
+                subtotal += price;
+                Console.WriteLine("Enter price of food item: [-1 to exit] ");
+                price = Convert.ToDouble(Console.ReadLine());
+
+
+            }
+
+            Console.WriteLine("How many people are in your party: ");
+            people = Convert.ToInt32(Console.ReadLine());
+
+            if (people >= 6)
+            {
+                gratuityAmount = subtotal * gratuityRate;
+            }
+
+            grandTotal = subtotal + gratuityAmount;
+
+            Console.WriteLine("Subtotal is: {0:C}", subtotal);
+            Console.WriteLine("{0:P} Gratuity: {1:C}", gratuityRate, gratuityAmount);
+            Console.WriteLine("Grand Total: {0:C}", grandTotal);
+
+        }
+    }
+}
---
+++ RestaurantTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RestaurantClass
+{
+    public class RestaurantTest
+    {
+        public static void Main(string[] args)
+        // Main method used to test Restaurant.cs 
+        {
+            Restaurant obj = new Restaurant("Dairy Queen", "123 Main St.", .15);
+
+            Console.WriteLine("{0}", obj.Name);
+            Console.WriteLine("{0}", obj.Address);
+
+            obj.GenerateReceipt();
+        }
+    }
+
+}
